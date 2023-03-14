80a0c896e3e36135b49bc99213e8f680e312a261
Mariia <utkaeva.maria@yandex.ru>
2023-03-14 14:49:02 +0300

    Task 1_1

---
+++ src/main/java/lesson_1/less_1_1.java
@@ -0,0 +1,37 @@
+package lesson_1;
+
+public class less_1_1 {
+    /**
+     Дан массив nums = [3,2,2,3] и число val = 3. Если в массиве есть числа, равные заданному,
+     нужно перенести эти элементы в конец массива.
+     Таким образом, первые несколько (или все) элементов массива
+     должны быть отличны от заданного, а остальные - равны ему.
+     */
+
+    public static void main(String[] args) {
+        int[] array = {1, 2, 2, 2, 1, 5, 3, 2, 3};
+        int val = 3;
+
+        swap(array, val);
+
+        for (int x : array) {
+            System.out.print(x + " ");
+        }
+    }
+    static void swap(int[] array, int val) {
+        int size = array.length;
+        int shift = 1;
+        for (int i = 0; i < size - shift; i++) {
+            while (array[size - shift] == val) {
+                shift++;
+            }
+
+            if (array[i] == val) {
+                int tmp = array[i];
+                array[i] = array[size - shift];
+                array[size - shift] = tmp;
+                shift++;
+            }
+        }
+    }
+}
